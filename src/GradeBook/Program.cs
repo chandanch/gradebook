e1232c865e24ad7310e27d4f4054bd9543402f5c
chandanch <chandan.cec107@gmail.com>
2020-03-22 08:46:47 +0530

    create method and field definition :art:

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,6 +8,7 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book();
+            book.AddGrade(12.343);
             var grades = new List<double>() { 10.21, 10.12, 10.52 };
             grades.Add(10.42);
 
