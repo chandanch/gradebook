9382c65bbfe0e8b00ee7a03519750c64dc5a4fde
chandanch <chandan.cec107@gmail.com>
2020-03-25 11:55:36 +0530

    test reference types :white_check_mark:

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -6,9 +6,20 @@ namespace GradeBook.Tests
     public class TypeTests
     {
         [Fact]
-        public void Test1()
+        public void GetBookReturnDistinctObjects()
         {
-           
+           var book1 = GetBook("Book1");
+           var book2 = GetBook("Book2");
+
+        //    Assert.Equal("Book1", book1.Name);
+        //    Assert.Equal("Book2", book2.Name);
+
+           Assert.NotEqual(book1, book2);
+        }
+
+        Book GetBook(string name)
+        {
+            return new Book(name);
         }
     }
 }
