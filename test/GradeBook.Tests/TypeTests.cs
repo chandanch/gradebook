4b96ff86bab87113858ac842ddf442ec865eea7a
chandanch <chandan.cec107@gmail.com>
2020-03-25 17:25:35 +0530

    working with strings :white_check_mark:

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -7,14 +7,15 @@ namespace GradeBook.Tests
     {
         
         [Fact]
-        public void ValueTypesPassByValue() {
+        public void ValueTypesPassByValue() 
+        {
             var x = GetInt();
             SetInt(ref x);
 
             Assert.Equal(42, x);
         }
 
-        private void SetInt(ref int x)
+        private void SetInt(ref Int32 x)
         {
             x = 42;
         }
@@ -25,7 +26,8 @@ namespace GradeBook.Tests
         }
 
         [Fact]
-        public void PassByReferencetoParameter() {
+        public void PassByReferencetoParameter() 
+        {
             var book1 = GetBook("Book1");
             SetNameByReference(out book1, "New Book");
             book1.Name = "Third Book";
@@ -40,7 +42,8 @@ namespace GradeBook.Tests
 
                 
         [Fact]
-        public void PassByValuetoParameter() {
+        public void PassByValuetoParameter() 
+        {
             var book1 = GetBook("Book1");
             GetSetName(book1, "New Book");
 
@@ -78,12 +81,38 @@ namespace GradeBook.Tests
         }
 
         [Fact]
-        public void TwoVarsReferenceSameObject() {
+        public void TwoVarsReferenceSameObject() 
+        {
             var book1 = new Book("Book1");
             var book2 = book1;
 
             Assert.Same(book1, book2);
             Assert.True(Object.ReferenceEquals(book1, book2));
+        }
+
+        [Fact]
+        public void StringBehaveLikeValueTypes() 
+        {
+            var name = "chandio";
+            var upper = ChangeToUpperCase(name);
+            var name2 = "JOHN";
+            ChangeToLowerCase(name2);
+
+            Assert.Equal("chandio", name);
+            Assert.Equal("CHANDIO", upper);
+            
+            Assert.Equal("JOHN", name2);
+        }
+
+        private string ChangeToUpperCase(string parameter)
+        {
+            return parameter.ToUpper();
+        }
+
+        private void ChangeToLowerCase(string parameter)
+        {
+            parameter = "QUIOR";
+            // return parameter;
         }
 
         Book GetBook(string name)
