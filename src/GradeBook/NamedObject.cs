9c839bd9be05b6e850a091ecbd822421ee1ab409
chandanch <chandan.cec107@gmail.com>
2020-04-11 10:18:12 +0530

    use of base class contructor :recycle:

--- src/GradeBook/NamedObject.cs
+++ src/GradeBook/NamedObject.cs
@@ -2,6 +2,11 @@ namespace GradeBook
 {
     public class NamedObject
     {
+        public NamedObject(string name)
+        {
+            Name = name;
+        }
+
         public string Name
         {
             get;
